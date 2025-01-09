a63b7e9b12708d8b06bb052dbbd9dbe8856a1834
Flavio Silva <fal_silva@hotmail.com>
2025-01-09 12:15:37 -0300

    criação das funções

--- script.js
+++ script.js
@@ -5,3 +5,38 @@ let descricao = document.querySelector('.d-1-4')
 let aviso = document.querySelector('.d-2')
 let conteudoLateral = document.querySelector('.d-1-right')
 let numeros = document.querySelector('.d-1-3')
+
+
+//Variaveis de controle de ambiente
+let etapaAtual = 0
+
+
+//Funções
+function comecarEtapa(){
+    let etapa = etapas[etapaAtual]
+
+    let numeroHTML = ''
+
+    seuVotoPara.style.display = 'none'
+    cargo.innerHTML = etapa.titulo
+    descricao.innerHTML = ''
+    aviso.style.display = 'none'
+    conteudoLateral.innerHTML = ''
+    numeros.innerHTML = numeroHTML
+}
+
+function clicou(num) {
+    alert('Clicou em num ' + num)
+}
+
+function branco(){
+    alert('Clicou no botão branco')
+}
+
+function corrigir(){
+    alert('Clicou no botão corrigir')
+}
+
+function confirmar(){
+    alert('Clicou no botão confirmar')
+}
